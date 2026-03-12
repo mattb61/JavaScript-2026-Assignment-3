9e9c2a98af3805735099d868bc74416f02f50e42
Matthew Brett <matthew.brett60@ed.cna.nl.ca>
2026-03-12 11:00:11 -0230

    Start of assignment 4

--- app/characters/[id]/page.jsx
+++ app/characters/[id]/page.jsx
@@ -1,5 +1,20 @@
 import Image from "next/image";
 
+
+// Fetch the data from the API and then use that data in the static params.
+const data = await fetch(cLink)
+    const characters = await data.json();
+    const characterName = characters["results"];
+
+export async function generateStaticParams() {
+    
+    const character = await fetch("https://rickandmortyapi.com/api/character/{id}").then((res) => res.json());
+
+    return character.map((char) => ({
+        slug: char.slug,
+    }))
+}
+
 export default async function CharacterDetail(props) {
 
     const {id} = await props.params;
